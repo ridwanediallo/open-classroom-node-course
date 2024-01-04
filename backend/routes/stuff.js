67c695980f001b8283304077e71a50f6b3e719bf
ridwan <ridwanediallo99@gmail.com>
2024-01-04 23:12:20 +0000

    Implement authentication middleware

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
 
-router.post('/', stuffCtrl.createThing);
-router.get('/', stuffCtrl.getAllStuff);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 
 module.exports = router;
